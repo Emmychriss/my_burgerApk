77220b5931d6747706a7c70add165890c628c608
Emmanuel <emmychris477@gmail.com>
2022-07-24 18:19:33 +0100

    burger ingredient props and state

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,10 +4,19 @@ import Aux from "../../higherOrderComponents/Auxilliary";
 import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      Cheese: 1,
+      Bacon: 1,
+      Salad: 2,
+      Meat: 1,
+    },
+  };
+
   render() {
     return (
       <Aux>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Controls</div>
       </Aux>
     );
