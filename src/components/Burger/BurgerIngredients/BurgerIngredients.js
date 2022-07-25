95d87e771ca0d54a38a1ab975056b24f0cf36d41
Emmanuel <emmychris477@gmail.com>
2022-07-25 19:11:30 +0100

    props spell correct; css modules import

--- src/components/Burger/BurgerIngredients/BurgerIngredients.js
+++ src/components/Burger/BurgerIngredients/BurgerIngredients.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
+import propTypes from "prop-types";
 
-import classes from "./BurgerIngredient.css";
+import classes from "./BurgerIngredients.css";
 
 class BurgerIngredient extends Component {
   render() {
@@ -34,7 +34,7 @@ class BurgerIngredient extends Component {
         break;
 
       case "Bacon":
-        ingredient = <div className={classes.Baconss}></div>;
+        ingredient = <div className={classes.Bacon}></div>;
         break;
 
       default:
@@ -46,8 +46,8 @@ class BurgerIngredient extends Component {
   }
 }
 
-BurgerIngredient.PropTypes = {
-  type: PropTypes.string.isRequired,
+BurgerIngredient.propTypes = {
+  type: propTypes.string.isRequired,
 };
 
 export default BurgerIngredient;
