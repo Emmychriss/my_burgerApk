391cdb11750e0b6e6520386ecc702a16bdfc075d
Emmanuel <emmychris477@gmail.com>
2022-07-25 19:07:24 +0100

    managing ingredients from state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,21 @@ import classes from "./Burger.css";
 import BurgerIngredient from "./BurgerIngredients/BurgerIngredients";
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients).map(
-    (ingKey) => {
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((ingKey) => {
       return [...Array(props.ingredients[ingKey])].map((_, i, arr) => {
-        return <BurgerIngredient key={ingKey + 1} type={ingKey} />;
+        return <BurgerIngredient key={ingKey + i} type={ingKey} />;
       });
-    }
-  );
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding some ingredients!</p>;
+  }
+
+  console.log(transformedIngredients);
 
   return (
     <div className={classes.Burger}>
