0c4f0abf8bf16a421316a02f7eced2daffd7c83e
Emmanuel <emmychris477@gmail.com>
2022-07-26 17:00:59 +0100

    build control(s) dynamics

---
+++ src/components/Burger/Buildcontrols/BuildControls.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import classes from "./BuildControls.css";
+import BuildControl from "./BuildControl/BuildControl";
+
+const controls = [
+  { ingLabel: "cheese", type: "Cheese" },
+  { ingLabel: "bacon", type: "Bacon" },
+  { ingLabel: "salad", type: "Salad" },
+  { ingLabel: "meat", type: "Meat" },
+];
+
+const buildControls = (props) => {
+  return (
+    <div className={classes.BuildControls}>
+      {controls.map((ctrl) => {
+        return (
+          <BuildControl
+            key={ctrl.ingLabel}
+            ingLabel={ctrl.ingLabel}
+            add={() => props.ingredientAdd(ctrl.type)}
+            remove={() => props.ingredientRemove(ctrl.type)}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default buildControls;
