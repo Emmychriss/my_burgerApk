689240fd778b0884308d54a29d78c9f45351f9cf
Emmanuel <emmychris477@gmail.com>
2022-07-29 17:39:32 +0100

    Action buttons on order summray

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,12 +1,14 @@
 import React from "react";
+
 import Aux from "../../../higherOrderComponents/Auxilliary";
 import classes from "./OrderSummary.css";
+import Button from "../../UI/Button/Button";
 
 const orderSummary = (props) => {
   const ingredientSummary = Object.keys(props.ingredients).map((ingKey) => {
     return (
       <li key={ingKey}>
-        <span style={{ textTransform: "capitlize" }}>{ingKey}</span> :
+        <span style={{ textTransform: "capitalize" }}>{ingKey}</span> <b>:</b>{" "}
         {props.ingredients[ingKey]}
       </li>
     );
@@ -18,7 +20,16 @@ const orderSummary = (props) => {
         <h3>Your Order</h3>
         <p>Your Order Consists of these ingredients: </p>
         <ul>{ingredientSummary}</ul>
+        <p>
+          <b>Total Price: {props.totalPrice.toFixed(2)}</b>
+        </p>
         <p>Proceed to Checkout?</p>
+        <Button clicked={props.purchaseCancel} btnType="Danger">
+          CANCEL
+        </Button>
+        <Button clicked={props.purchaseContinue} btnType="Success">
+          CONTINUE
+        </Button>
       </div>
     </Aux>
   );
